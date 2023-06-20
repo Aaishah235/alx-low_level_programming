641897f2b070747de5ec47cdbcef0f5b516fa870
Aaishah235 <pearlhumayrah235@gmail.com>
2023-06-19 17:40:23 -0700

    Combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (dgt1 = 0; dgt1 < 9; dgt1++)
 	{
-		for (dgt2 = 0; dgt2 < 10; dgt2++)
+		for (dgt2 = dgt1 + 1; dgt2 < 10; dgt2++)
 		{
 			putchar((dgt1 % 10) + '0');
 			putchar((dgt2 % 10) + '0');
 
-			if (dgt1 == 9 && dgt2 == 9)
+			if (dgt1 == 8 && dgt2 == 9)
 				continue;
 
 			putchar(',');
