f07381270b86a58e4d9759162da5b434ebb64047
Aaishah235 <pearlhumayrah235@gmail.com>
2023-06-19 17:31:04 -0700

    Combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible different combinations of two-digits
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int dgt1, dgt2;
+
+	for (dgt1 = 0; dgt1 < 10; dgt1++)
+	{
+		for (dgt2 = 0; dgt2 < 10; dgt2++)
+		{
+			putchar((dgt1 % 10) + '0');
+			putchar((dgt2 % 10) + '0');
+
+			if (dgt1 == 9 && dgt2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
