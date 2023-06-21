4f53c597461ae6f878884cdd9890e52f2a33d2f3
Aaishah235 <pearlhumayrah235@gmail.com>
2023-06-21 12:16:33 -0700

    Print 10times lowercase alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,15 @@ void print_alphabet_x10(void)
 {
 
 	char c;
-	int i = 0;
+	int b = 0;
 
-	while (i <= 9)
+	while (b <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 		_putchar(c);
 		}
 		_putchar('\n');
-		i++;
+		b++;
 	}
 }
