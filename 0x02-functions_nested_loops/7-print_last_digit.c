762553800f61aa531aeb7914b980aa883c86d430
Aaishah235 <pearlhumayrah235@gmail.com>
2023-06-25 09:29:10 -0700

    Print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int b)
 {
 	int lastd;
 
-	lastd = b % 10;
+		lastd = b % 10;
 
 	if (lastd < 0)
 	{
-	lastd = lastd * -1;
+		lastd = lastd * -1;
 	}
 	_putchar(lastd + '0');
 	return (lastd);
